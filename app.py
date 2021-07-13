437f0483337512982cfca22e8410de1511418ad2
robertzinzun <robertzinzun@gmail.com>
2021-07-13 14:01:25 -0500

    Persistencia de la Tabla Categorias
    Implementando la persistencia de los datos en la tabla Categorías

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_bootstrap import Bootstrap
+from modelo.Dao import db,Categoria
 app = Flask(__name__)
 Bootstrap(app)
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://user_shopitesz:Shopit3sz.123@localhost/shopitesz'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.secret_key='Cl4v3'
 @app.route("/")
 def inicio():
     #return "Bienvenido a la tienda en linea Shopitesz"
@@ -48,6 +51,58 @@ def consultarCliente(nombre):
 def consultarPorductosPorPrecio(precio):
     return "Hola"+str(precio)
 
+#CRUD de Categorias
+@app.route('/Categorias')
+def consultaCategorias():
+    cat=Categoria()
+    return render_template('categorias/consultaGeneral.html',categorias=cat.consultaGeneral())
+
+@app.route('/Categorias/consultarImagen/<int:id>')
+def consultarImagenCategoria(id):
+    cat=Categoria()
+    return cat.consultarImagen(id)
+
+@app.route('/Categorias/nueva')
+def nuevaCategoria():
+    return render_template('categorias/agregar.html')
+
+@app.route('/Categorias/agregar',methods=['post'])
+def agregarCategoria():
+    try:
+        cat=Categoria()
+        cat.nombre=request.form['nombre']
+        cat.imagen=request.files['imagen'].stream.read()
+        cat.estatus='Activa'
+        cat.agregar()
+        flash('¡ Categoria agregada con exito !')
+    except:
+        flash('¡ Error al agregar la categoria !')
+    return redirect(url_for('consultaCategorias'))
+
+@app.route('/Categorias/<int:id>')
+def consultarCategoria(id):
+    cat=Categoria()
+    return render_template('categorias/editar.html',cat=cat.consultaIndividuall(id))
+
+@app.route('/Categorias/editar',methods=['POST'])
+def editarCategoria():
+    try:
+        cat=Categoria()
+        cat.idCategoria=request.form['id']
+        cat.nombre=request.form['nombre']
+        imagen=request.files['imagen'].stream.read()
+        if imagen:
+            cat.imagen=imagen
+        cat.estatus=request.values.get("estatus","Inactiva")
+        cat.editar()
+        flash('¡ Categoria editada con exito !')
+    except:
+        flash('¡ Error al editar la categoria !')
+
+    return redirect(url_for('consultaCategorias'))
+    
+#Fin del crud de categorias
 if __name__=='__main__':
+    db.init_app(app)#Inicializar la BD - pasar la configuración de la url de la BD
     app.run(debug=True)
 
---
+++ modelo/Dao.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column,Integer,String,BLOB
+
+db=SQLAlchemy()
+
+class Categoria(db.Model):
+    __tablename__='Categorias'
+    idCategoria=Column(Integer,primary_key=True)
+    nombre=Column(String,unique=True)
+    estatus=Column(String,nullable=False)
+    imagen=Column(BLOB)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividuall(self,id):
+        return Categoria.query.get(id)
+
+    def consultarImagen(self,id):
+        return self.consultaIndividuall(id).imagen
+
+    def agregar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def editar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
